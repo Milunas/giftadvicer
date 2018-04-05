b87d49bf5a25f1cfdb4c3932fed156ed2ef7fce5
Łukasz Milunas <milunas.lukasz@gmail.com>
2018-04-05 11:19:06 +0200

    Fix intent extra

--- mobile/app/src/main/java/milunas/giftadvice/ui/activity/CreateGroupActivity.kt
+++ mobile/app/src/main/java/milunas/giftadvice/ui/activity/CreateGroupActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import milunas.giftadvice.R
 import milunas.giftadvice.model.Group
 import milunas.giftadvice.provider.UserProvider
@@ -17,16 +21,22 @@ class CreateGroupActivity : AppCompatActivity() {
 
         val group = Group(null,"Coco", null, null)
         val btn = findViewById<Button>(R.id.add_group_btn)
-
         btn.setOnClickListener {
-            UserProvider.addNewGroup(buildUrl(), group)
+            addGroup(group)
             Toast.makeText(this, "Created!", Toast.LENGTH_SHORT).show()
         }
     }
 
+    private fun addGroup(group: Group) = launch(UI){
+        val task = async(CommonPool) {
+            UserProvider.addNewGroup(buildUrl(),group)
+        }
+        val response = task.await()
+        println(response)
+    }
+
     private fun buildUrl() : String{
-        val userId = intent.getStringExtra("Localization")
-        return baseUrl + userId + "/group"
+        return baseUrl + intent.getStringExtra("USER_ID") + "/group"
     }
 
 }
